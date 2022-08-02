70e1bae43c4734c55c46b500a598a11dc3c31b96
Lasse Benninga <devops.pipeline@example.com>
2022-08-03 00:31:18 +0200

    try one reviews

--- main.py
+++ main.py
@@ -4,23 +4,27 @@ from scraper.review import scrape_reviews
 from models.reviews import Reviews
 
 if __name__ == '__main__':
-    restaurants = scrape_restaurants()
-    all_reviews = []
-    for restaurant in restaurants:
-        reviews = scrape_reviews(restaurant.id)
+    reviews = scrape_reviews("003P3N31")
+    print(reviews)
+    # restaurants = scrape_restaurants()
+    # all_reviews = []
+    # for restaurant in restaurants:
+    #     reviews = scrape_reviews(restaurant.id)
 
-        for review in reviews.reviews:
-            if review.comment == "":
-                continue
+    #     for review in reviews.reviews:
+    #         if review.comment == "":
+    #             continue
 
-            review.restaurant_name = restaurant.primary_slug
-            review.rating_food = review.rating.food
-            review.rating_delivery = review.rating.delivery
-            del review.rating
+    #         review.restaurant_name = restaurant.primary_slug
+    #         review.rating_food = review.rating.food
+    #         review.rating_delivery = review.rating.delivery
+    #         del review.rating
 
-            all_reviews.append(review)
+    #         all_reviews.append(review)
+    #         break
+    #     break
 
-    res = Reviews(reviews=all_reviews)
+    # res = Reviews(reviews=all_reviews)
 
-    with open('data/reviews.json', 'w') as f:
-        f.write(res.json())
+    # with open('data/reviews.json', 'w') as f:
+    #     f.write(res.json())
