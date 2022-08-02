9bff9329f421c44f4918b240f93167e380125e90
Lasse Benninga <devops.pipeline@example.com>
2022-08-02 21:34:46 +0200

    Add main

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+url = "https://cw-api.takeaway.com/api/v31/restaurants?deliveryAreaId=936874&postalCode=3511&lat=52.08944030000001&lng=5.1099869&limit=0&isAccurate=false"
+
+payload={}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:102.0) Gecko/20100101 Firefox/102.0',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Referer': 'https://www.thuisbezorgd.nl/',
+    'X-Language-Code': 'en',
+    'X-Country-Code': 'nl',
+    'X-Session-ID': '8070b889-a1bc-4205-a2cc-4aed0a3c8e50',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Origin': 'https://www.thuisbezorgd.nl',
+    'Connection': 'keep-alive',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'cross-site',
+    'TE': 'trailers',
+    'Cookie': '__cf_bm=VthKD.EertRh30SheMdiRtk8nOEya9_wMqymvmyT5Qg-1659468095-0-ASKMDZSW+hnKw1B4NRPjLVigazSp4UbzC1MXNINpjAY20A+jtRyPtdxu1/U7yp9nfbJ0SAklaEvyIcyZi8C0GKrr3q2STvdUPLsgPB8y+BRX'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+restaurants = json.loads(response.text)['restaurants']
+
+with open('restaurants.json','w') as f:
+    f.write(json.dumps(restaurants))
+    
