e54dc25dbf4904cc2c62c8692c6a4c053fcdb12d
Lasse Benninga <devops.pipeline@example.com>
2022-08-03 00:44:19 +0200

    Use hardcoded mappings

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
+import json
 from scraper.restaurant import scrape_restaurants
 from scraper.review import scrape_reviews
 
 from models.reviews import Reviews
 
 if __name__ == '__main__':
-    restaurants = scrape_restaurants()
-    all_reviews = []
-    for restaurant in restaurants:
-        reviews = scrape_reviews(restaurant.id)
+    with open('data/mapping.json', 'r') as f:
+        mapping = json.load(f)
+        all_reviews = []
+        for restaurant_id,restaurant_name in mapping.items():
+            reviews = scrape_reviews(restaurant_id)
 
-        for review in reviews.reviews:
-            if review.comment == "":
-                continue
+            for review in reviews.reviews:
+                if review.comment == "":
+                    continue
 
-            review.restaurant_name = restaurant.primary_slug
-            review.rating_food = review.rating.food
-            review.rating_delivery = review.rating.delivery
-            del review.rating
+                review.restaurant_name = restaurant_name
+                review.rating_food = review.rating.food
+                review.rating_delivery = review.rating.delivery
+                del review.rating
 
-            all_reviews.append(review)
+                all_reviews.append(review)
+                break
             break
-        break
 
-    res = Reviews(reviews=all_reviews)
+        res = Reviews(reviews=all_reviews)
 
-    with open('data/reviews.json', 'w') as f:
-        f.write(res.json())
+        with open('data/reviews.json', 'w') as f:
+            f.write(res.json())
